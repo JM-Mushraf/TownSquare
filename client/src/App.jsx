df1feab06258cfef1834b5a9f9dd4e7c1cc7fa78
Himanshu Sahu <himpreetak@gmail.com>
2025-05-11 23:40:14 +0530

    dfssdf

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ axios.defaults.withCredentials = true
 
 // Lazy load all page components
 const HomePage = lazy(() => import("./Pages/HomePage/HomePage.jsx"))
-const DiscussionsPage = lazy(() => import("./Pages/DiscussionsPage.jsx"))
+const DiscussionsPage = lazy(() => import("./Pages/Discussions/DiscussionsPage.jsx"))
 const AnnouncementsPage = lazy(() => import("./Pages/Announcements/AnnouncementsPage.jsx"))
 // const LeaderboardPage = lazy(() => import("./pages/LeaderboardPage"))
 const UserProfile = lazy(() => import("./Pages/UserProfile/UserProfile.jsx"))
