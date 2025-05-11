b9427c503e34fa8dc87d9ee13369b79a10be7e22
Himanshu Sahu <himpreetak@gmail.com>
2025-05-11 20:26:17 +0530

    vite config change

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,26 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return
+        warn(warning)
+      },
+      output: {
+        manualChunks: {
+          mui: ['@mui/material', '@mui/icons-material'],
+          framer: ['framer-motion'],
+          vendor: ['react', 'react-dom', 'react-router-dom']
+        }
+      }
+    }
+  },
+  css: {
+    devSourcemap: true
+  }
 })
