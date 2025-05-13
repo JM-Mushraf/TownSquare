154d4e24f2e6ac34bcfb76252d9eedb1e0511c63
JM-Mushraf <mushraf1786@gmail.com>
2025-05-13 15:18:58 +0530

    njnjs

--- client/src/Apis/postApi.jsx
+++ client/src/Apis/postApi.jsx
@@ -3,9 +3,6 @@ import axios from 'axios'
 export const fetchAnnouncements = async () => {
     try {
       const {data} = await axios.get(`${import.meta.env.VITE_BACKEND_BASEURL}/post/announcements/all`); // Replace with your backend endpoint
-      
-      
-      
       return data.announcements;
     } catch (error) {
       console.error('Error fetching announcements:', error);
